38805ad6227b9cdf8e3524d614d99c9573ec34e8
Diegorinio <zbyslaf321@gmail.com>
2025-11-16 01:30:53 +0100

    Added working button in ItemElemnt

--- assets/components/Restartly.js
+++ assets/components/Restartly.js
@@ -1,7 +1,7 @@
 import { GameObject, Rect,Text,Button} from "./Classes";
 import { COLORS } from "./colors";
 import * as hmUI from "@zos/ui";
-import { log as Logger } from "@zos/utils";
+import { log, log as Logger } from "@zos/utils";
 export class ItemElement extends GameObject{
   constructor(x,y,width,height,item=null){
     super(x,y,width,height);
@@ -17,6 +17,7 @@ export class ItemElement extends GameObject{
     this.resetButton=null;
     this.editButton=null;
     this.Widgets=[this.background];
+    this.Active=false;
     if(item!=null){
       this.assignedItem=item;
       Logger.debug(item.title);
@@ -35,15 +36,39 @@ export class ItemElement extends GameObject{
       const left=this.width*0.20;
       const middle=this.width*0.50;
       const right=this.width*0.30;
-      this.titleLabel=new Text(left,this.y,middle,this.height/2,this.height/4,item.title,COLORS.WHITE,null,hmUI.align.LEFT);
+      this.titleLabel=new Text(left-50,this.y,left,this.height,this.height/4,item.title,COLORS.WHITE,hmUI.align.CENTER_V,hmUI.align.LEFT);
 
       this.timerLabel=new Text(this.titleLabel.x+this.titleLabel.width,this.y,middle,this.height,this.height/4,item.time,COLORS.WHITE,null);
-      this.Widgets.push(this.titleLabel,this.timerLabel);
+
+      this.startButton=new Button(left+middle,this.y,100,this.height,"EDIT",COLORS.RED,COLORS.BLACK,null,this.ToggleTimer,12);
+      this.Widgets.push(this.titleLabel,this.timerLabel,this.startButton);
     }
-  Draw=()=>{
-    this.Widgets.forEach(el=>{
-      el.Draw();
-    })
+
+    Draw=()=>{
+      this.Widgets.forEach(el=>{
+        el.Draw();
+      })
+    }
+
+    Update=()=>{
+      this.timerLabel.SetText(this.time);
+      this.Draw();
+    }
+
+    OnTick=()=>{
+      if(this.Active){
+        this.time+=1;
+        this.Update();
+      }
+    }
+  StopTimer=()=>{
+    this.Active=false;
+  }
+  StartTimer=()=>{
+    this.Active=true;
+  }
+  ToggleTimer=()=>{
+    this.Active=!this.Active;
   }
 }
 export class Item{
--- page/gt/home/index.page.js
+++ page/gt/home/index.page.js
@@ -1,5 +1,5 @@
 import * as hmUI from "@zos/ui";
-import { log as Logger } from "@zos/utils";
+import { log, log as Logger } from "@zos/utils";
 import * as GameObject from "../../../assets/components/Classes";
 import {COLORS} from "../../../assets/components/colors";
 import { GameLoop } from "../../../assets/components/GameLoop";
@@ -21,13 +21,14 @@ Page({
     let xd=1;
     logger.debug("page build invoked");
     const text = new GameObject.Text(100,100,50,50,20,"sss",COLORS.AMBER);
-    const itttem=new RTLY.Item(0,"Test",100);
+    const itttem=new RTLY.Item(0,"Test",0);
     const itemElement=new RTLY.ItemElement(0,0,DEVICE_WIDTH,100,itttem);
     itemElement.Draw();
     text.Draw();
     GlobalLoop.OnTick(()=>{
       text.SetText(xd);
       text.Draw();
+      itemElement.OnTick();
       xd++;
     })
 
