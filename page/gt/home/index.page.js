3e59fc2e6a3f7ba39922ebc994af7f74d96b6710
Diegorinio <zbyslaf321@gmail.com>
2025-11-16 15:05:20 +0100

    Fixed ViewContiner position to fix under logo, rounded button, added button event to add new item

--- page/gt/home/index.page.js
+++ page/gt/home/index.page.js
@@ -9,8 +9,9 @@ const GlobalLoop=GameLoop.getInstance();
 GlobalLoop.SetTick(1000);
 const logger = Logger.getLogger("helloworld");
 const LOGO=new GameObject.Text(0,0,DEVICE_WIDTH,50,32,"Restartly",COLORS.RED,hmUI.align.BOTTOM,hmUI.align.CENTER_H);
-const newItemButton=new GameObject.Button(0,DEVICE_HEIGHT-100,DEVICE_WIDTH,100,"ADD NEW ITEM",COLORS.WHITE,COLORS.BLUE,null);
-const ItemContainer=new GameObject.Container();
+const newItemButton=new GameObject.Button(0,DEVICE_HEIGHT-105,DEVICE_WIDTH,100,"ADD NEW ITEM",COLORS.WHITE,COLORS.DARK_BLUE,null,CreateNewItem,60);
+const ItemContainer=new GameObject.ViewContainer(0,LOGO.y+LOGO.height,DEVICE_WIDTH,(DEVICE_HEIGHT-LOGO.height),[],true,-1);
+let itemElements=[];
 let items=[];
 Page({
   style:{
@@ -24,18 +25,20 @@ Page({
     LOGO.Draw();
     let xd=1;
     logger.debug("page build invoked");
-    const VContainer=new GameObject.ViewContainer(0,LOGO.y,DEVICE_WIDTH,(DEVICE_HEIGHT-LOGO.height),[],true);
     const text = new GameObject.Text(100,100,50,50,20,"sss",COLORS.AMBER);
     const itttem=new RTLY.Item(0,"Test",0);
-    const itemElement=new RTLY.ItemElement(0,100,DEVICE_WIDTH,100,itttem);
-    VContainer.AddWidget(itemElement);
-    VContainer.InitializeWidgets();
-    VContainer.AddScrollSpacer(1);
+    const itemElement=new RTLY.ItemElement(0,0,DEVICE_WIDTH,100,itttem);
+    itemElements.push(itemElement);
+    ItemContainer.AddWidget(itemElement);
+    ItemContainer.InitializeWidgets();
+    ItemContainer.AddScrollSpacer(1);
     // itemElement.Draw();
     GlobalLoop.OnTick(()=>{
       // text.SetText(xd);
       // text.Draw();
-      itemElement.OnTick();
+      itemElements.forEach(el=>{
+        el.OnTick();
+      })
     })
 
     newItemButton.Draw();
@@ -46,6 +49,12 @@ Page({
   },
 });
 
-function CreateNewItem(item){
-
+function CreateNewItem(){
+  new_item=new RTLY.Item(items.length+1,"Item "+items.length+1,0);
+  items.push(new_item);
+  const last_item=itemElements[items.length-1];
+  const new_itemElement=new RTLY.ItemElement(0,last_item.y+last_item.height+5,last_item.width,last_item.height,new_item);
+  itemElements.push(new_itemElement)
+  ItemContainer.AddWidget(new_itemElement);
+  ItemContainer.InitializeWidgets();
 }
