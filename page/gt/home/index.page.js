dd9f4066f68e5399ae8af9b568e7d3e9811e844b
Diegorinio <zbyslaf321@gmail.com>
2025-12-04 20:53:22 +0100

    added timer with reset, todo: history

--- page/gt/home/index.page.js
+++ page/gt/home/index.page.js
@@ -8,20 +8,26 @@ import { GameLoop } from "../../../assets/components/GameLoop";
 import { DEVICE_WIDTH,DEVICE_HEIGHT } from "./index.page.r.layout";
 import { getText } from "@zos/i18n";
 import * as RTLY from "../../../assets/components/Restartly";
-import { LocalStorage } from '@zos/storage'
+import { localStorage } from '@zos/storage'
+import { createModal, MODAL_CONFIRM} from "@zos/interaction";
 const GlobalLoop=GameLoop.getInstance();
 GlobalLoop.SetTick(1000);
 const logger = Logger.getLogger("helloworld");
 
 const LOGO=new GameObject.Text(0,0,DEVICE_WIDTH,80,52,getText('appName'),COLORS.RED,hmUI.align.BOTTOM,hmUI.align.CENTER_H);
+
 const logoUnderText=new GameObject.Text(0,LOGO.height,DEVICE_WIDTH,30,24,getText('appTitle'),COLORS.RED,hmUI.align.BOTTOM,hmUI.align.CENTER_H);
+const noSmokeLabel=new GameObject.Text(0,DEVICE_HEIGHT/3,DEVICE_WIDTH,28,22,getText("no-smoking-text"),COLORS.RED);
+
+const TimerText=new GameObject.Text(0,noSmokeLabel.y,DEVICE_WIDTH,150,52,"Press to start",COLORS.WHITE);
+
+const toggleTimerButton=new GameObject.Button(50,DEVICE_HEIGHT-105,DEVICE_WIDTH-100,100,getText("toggleButton-start"),COLORS.WHITE,COLORS.TEAL,null,ToggleTimer,60,null,99);
 
 const settingsButton=new GameObject.ImageButton(LOGO.x,LOGO.y,LOGO.width,LOGO.height,"",COLORS.NAVY_BLUE,getText("options-icon"),null,GoToSettingsPage);
-const resetTimerButton=new GameObject.Button(50,DEVICE_HEIGHT-105,DEVICE_WIDTH-100,100,"+",COLORS.WHITE,COLORS.TEAL,null,StartTimer,60,null,99);
 
-const TimerText=new GameObject.Text(0,DEVICE_HEIGHT/2-100,DEVICE_WIDTH,150,52,"Press to start",COLORS.WHITE);
 const _Time=new Time();
-const timer={start:_Time.getTime()}
+const timer={start:_Time.getTime(),isActive:false}
+logger.log(JSON.stringify(timer));
 
 Page({
   style:{
@@ -29,35 +35,68 @@ Page({
   },
   onInit(params) {
     hmUI.setStatusBarVisible(false);
+    const savedTimer=GetLocalStorageTimer();
+    if(savedTimer){
+      timer.start=savedTimer.start;
+      timer.isActive=savedTimer.isActive;
+      logger.log("Load from save");
+      logger.log(JSON.stringify(timer));
+      toggleTimerButton.SetText(getText("toggleButton-reset"));
+    }
     logger.debug("page onInit invoked");
   },
   build() {
-    // hmRoute.push({url:"/page/gt/home/index.page.select_date"});
     LOGO.Draw();
     logoUnderText.Draw();
+    
     TimerText.Draw();
     settingsButton.Draw();
-    resetTimerButton.Draw();
+    toggleTimerButton.Draw();
+    noSmokeLabel.Draw();
     GlobalLoop.OnTick(CalculateTime)
     GlobalLoop.Start();
   },
   onDestroy() {
     logger.debug("page onDestroy invoked");
+    Save();
   },
 });
-const x = "chuj".tposx
+
+function ToggleTimer(){
+  if(!timer.isActive){
+    StartTimer();
+  }
+  else{
+    const dialog=createModal({
+            content:"Do you want to reset?",
+            autoHide:false,
+            onClick:(keyObj)=>{
+              const {type}=keyObj
+              if(type==MODAL_CONFIRM){
+                timer.isActive=false;
+                StartTimer();
+              }
+              else{
+
+              }
+              dialog.show(false);
+    }
+  })
+  }
+}
 function StartTimer(){
     const now=new Time().getTime();
     timer.start=now;
-}
-function ResetTimer(){
-    timer.start=0;
+    timer.isActive=true;
+    toggleTimerButton.SetText(getText("toggleButton-reset"));
 }
 function CalculateTime(){
+  if(timer.isActive){
     const now=new Time().getTime();
     const diff=now-timer.start;
     const _timeString=formatTime(diff);
     TimerText.SetText(_timeString);
+  }
 }
 function GoToSettingsPage(){
   hmRoute.push({url:"/page/gt/home/index.page.settings"});
@@ -83,4 +122,19 @@ function formatTime(ms) {
     if (s > 0 || parts.length === 0) parts.push(`${s}s`);
 
     return parts.join(" ");
+}
+
+function Save(){
+  logger.log(JSON.stringify(timer));
+  localStorage.setItem("timer",JSON.stringify(timer))
+}
+
+function GetLocalStorageTimer(){
+  const storedTimer=localStorage.getItem("timer");
+  if(storedTimer){
+    return JSON.parse(storedTimer);
+  }
+  else{
+    return timer;
+  }
 }
