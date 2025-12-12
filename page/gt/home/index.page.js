0f277a9a04767a6a91763cfb51ddfc5e460d7f50
Diegorinio <zbyslaf321@gmail.com>
2025-12-12 23:01:44 +0100

    Added one smoke button, added history of resets, added smoke count per day

--- page/gt/home/index.page.js
+++ page/gt/home/index.page.js
@@ -17,23 +17,30 @@ const logger = Logger.getLogger("helloworld");
 const LOGO=new GameObject.Text(0,0,DEVICE_WIDTH,80,52,getText('appName'),COLORS.RED,hmUI.align.BOTTOM,hmUI.align.CENTER_H);
 
 const logoUnderText=new GameObject.Text(0,LOGO.height,DEVICE_WIDTH,30,24,getText('appTitle'),COLORS.RED,hmUI.align.BOTTOM,hmUI.align.CENTER_H);
-const noSmokeLabel=new GameObject.Text(0,DEVICE_HEIGHT/3,DEVICE_WIDTH,28,22,getText("no-smoking-text"),COLORS.RED);
+const noSmokeLabel=new GameObject.Text(0,DEVICE_HEIGHT/3,DEVICE_WIDTH,28,22,getText("no-smoking-text"),COLORS.GREEN);
 
-const TimerText=new GameObject.Text(0,noSmokeLabel.y,DEVICE_WIDTH,150,52,"Press to start",COLORS.WHITE);
+const TimerText=new GameObject.Text(0,noSmokeLabel.y,DEVICE_WIDTH,150,48,"Press to start",COLORS.WHITE);
 
-const toggleTimerButton=new GameObject.Button(50,DEVICE_HEIGHT-105,DEVICE_WIDTH-100,100,getText("toggleButton-start"),COLORS.WHITE,COLORS.TEAL,null,ToggleTimer,60,null,99);
+const smokesTodayLabel=new GameObject.Text(DEVICE_WIDTH/5,TimerText.y+TimerText.height-15,DEVICE_WIDTH/2,32,25,getText("streak"),COLORS.RED,hmUI.align.CENTER_V,hmUI.align.RIGHT);
 
-const settingsButton=new GameObject.ImageButton(LOGO.x,LOGO.y,LOGO.width,LOGO.height,"",COLORS.NAVY_BLUE,getText("options-icon"),null,GoToSettingsPage);
+const todaySmokeAmount=new GameObject.Text(smokesTodayLabel.x+smokesTodayLabel.width,smokesTodayLabel.y,DEVICE_WIDTH/2+15,32,25,"0",COLORS.RED,hmUI.align.CENTER_V,hmUI.align.LEFT);
+
+const toggleTimerButton=new GameObject.Button(50,DEVICE_HEIGHT-105,DEVICE_WIDTH-100,100,getText("toggleButton-start"),COLORS.DARK_BLUE,COLORS.RED,null,ToggleTimer,42,null,52);
 
 const _Time=new Time();
 const timer={start:_Time.getTime(),isActive:false}
-logger.log(JSON.stringify(timer));
+//Format {day:dd,month:dd,year:yyyy,hour:hh,minutes:mm}
+const resetHistory={history:[]}
+function UpdatAmountEvent(){
+  UpdateAmount(todaySmokeAmount);
+}
 
 Page({
   style:{
     titleBar:false
   },
   onInit(params) {
+    // localStorage.clear();
     hmUI.setStatusBarVisible(false);
     const savedTimer=GetLocalStorageTimer();
     if(savedTimer){
@@ -43,14 +50,23 @@ Page({
       logger.log(JSON.stringify(timer));
       toggleTimerButton.SetText(getText("toggleButton-reset"));
     }
+    const savedHitory=GetLocalStorageHistory();
+    logger.log(JSON.stringify(savedHitory));
+    if(savedHitory){
+      resetHistory.history=savedHitory.history;
+      logger.log(JSON.stringify(resetHistory));
+    }
     logger.debug("page onInit invoked");
   },
   build() {
     LOGO.Draw();
     logoUnderText.Draw();
-    
     TimerText.Draw();
-    settingsButton.Draw();
+    smokesTodayLabel.Draw();
+    todaySmokeAmount.Draw();
+    const smokesToday=GetSmokesAmountToday();
+    if(smokesToday)
+      todaySmokeAmount.SetText(smokesToday.toString());
     toggleTimerButton.Draw();
     noSmokeLabel.Draw();
     GlobalLoop.OnTick(CalculateTime)
@@ -73,7 +89,6 @@ function ToggleTimer(){
             onClick:(keyObj)=>{
               const {type}=keyObj
               if(type==MODAL_CONFIRM){
-                timer.isActive=false;
                 StartTimer();
               }
               else{
@@ -86,9 +101,19 @@ function ToggleTimer(){
 }
 function StartTimer(){
     const now=new Time().getTime();
+    if(timer.isActive==true){
+      timer.isActive=false;
+      Reset();
+    }
     timer.start=now;
     timer.isActive=true;
     toggleTimerButton.SetText(getText("toggleButton-reset"));
+}
+function Reset(){
+  const now = new Time();
+  const _reset={day:now.getDate().toString(),month:now.getMonth().toString(),year:now.getFullYear().toString(),hours:now.getHours().toString(),minutes:now.getMinutes().toString(),start:timer.start,end:now.getTime()};
+  AddToLocalStorageHistory(_reset);
+  UpdatAmountEvent();
 }
 function CalculateTime(){
   if(timer.isActive){
@@ -97,9 +122,6 @@ function CalculateTime(){
     const _timeString=formatTime(diff);
     TimerText.SetText(_timeString);
   }
-}
-function GoToSettingsPage(){
-  hmRoute.push({url:"/page/gt/home/index.page.settings"});
 }
 function formatTime(ms) {
     // zamiana ms na sekundy
@@ -135,6 +157,52 @@ function GetLocalStorageTimer(){
     return JSON.parse(storedTimer);
   }
   else{
-    return timer;
+    return null;
   }
+}
+
+function GetLocalStorageHistory(){
+  const _history=localStorage.getItem(getText("history"));
+  if(_history){
+    return JSON.parse(_history);
+  }
+  else{
+    return null;
+  }
+}
+
+function AddToLocalStorageHistory(itemDate){
+  const _local=localStorage.getItem(getText("history"));
+  if(!_local){
+    const _local={history:[]}
+    localStorage.setItem(getText("history"),JSON.stringify(_local));
+  }
+  const storage=JSON.parse(localStorage.getItem(getText("history")));
+  let _storageArray=storage.history;
+  _storageArray.push(itemDate);
+  storage.history=_storageArray;
+  localStorage.setItem(getText("history"),JSON.stringify(storage));
+}
+
+function GetSmokesAmountToday(){
+  const stored=GetLocalStorageHistory();
+  if(!stored||!stored.history||stored.history.length===0){
+    return null;
+  }
+  let amountToday=0
+  const _currentTime= new Time();
+  stored.history.forEach(el=>{
+    if(el.day==_currentTime.getDate()){
+      amountToday+=1;
+    }
+  })
+  return amountToday
+}
+
+function UpdateAmount(label){
+  if(!label){
+    return;
+  }
+  const _amount=GetSmokesAmountToday();
+  label.SetText(_amount.toString());
 }
